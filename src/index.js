d3137c8a0c824204b60d98d3021984eac030d10a
karta2341940 <a08487a@gmail.com>
2022-03-01 16:11:14 +0800

    lesson1

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let navlink = document.getElementsByClassName('nav-link');
+console.log(navlink.length);
+for (let i = 0; i < navlink.length; i++) {
+    navlink[i].addEventListener('click', (e) => {
+        for (let i = 0; i < navlink.length; i++) {
+            navlink[i].classList.remove('active');    
+        }
+        navlink[i];
+        //this.classList.add('active');
+    });
+}
+
+
+let form = document.getElementsByTagName('form')[0];
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        ds =document.getElementById('inputText').value.trim()
+        console.log(ds);
+    });
+});
