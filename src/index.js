5ef2a75d3be508ad9ba6fd0eff4b418257cd5647
karta2341940 <a08487a@gmail.com>
2022-03-01 16:17:34 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ console.log(navlink.length);
 for (let i = 0; i < navlink.length; i++) {
     navlink[i].addEventListener('click', (e) => {
         for (let i = 0; i < navlink.length; i++) {
-            navlink[i].classList.remove('active');    
+            navlink[i].classList.remove('active');
         }
         navlink[i].classList.add('active');
         //this.classList.add('active');
@@ -13,11 +13,21 @@ for (let i = 0; i < navlink.length; i++) {
 
 let form = document.getElementsByTagName('form')[0];
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
-    form.addEventListener('submit',(e)=>{
+    form.addEventListener('submit', (e) => {
         e.preventDefault();
-        ds =document.getElementById('inputText').value.trim()
-        console.log(ds);
+        ds = document.getElementById('inputText').value.trim()
+        if (ds.length == 0) {
+            alert('Please input task')
+        }
+        else { 
+            const item={
+                name:ds,
+                isDone:false,
+                time: new Date()
+            }
+        }
+        console.log(item);
     });
 });
