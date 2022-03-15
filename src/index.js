970203e897074832fd3fc4f01b1c94d27897b7a3
karta2341940 <38270318+karta2341940@users.noreply.github.com>
2022-03-15 16:16:41 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let navlink = document.getElementsByClassName('nav-link');
-console.log("navlink.length:",navlink.length);
+console.log("navlink.length:", navlink.length);
 for (let i = 0; i < navlink.length; i++) {
     navlink[i].addEventListener('click', (e) => {
         for (let i = 0; i < navlink.length; i++) {
@@ -10,62 +10,75 @@ for (let i = 0; i < navlink.length; i++) {
     });
 }
 
-let store=[];
-let setLS = (item)=>{
-    if(item.length > 0 )
-    localStorage.setItem("itemLocal",JSON.stringify(item));
+let store = [];
+let setLS = (item) => {
+    if (item.length > 0)
+        localStorage.setItem("item-all", JSON.stringify(item));
     else console.log("this data is null");
 }
 
-let getLs = ()=>{
-    return JSON.parse(localStorage.getItem("itemLocal"));
+let getLs = () => {
+    return JSON.parse(localStorage.getItem("item-all"));
 }
-let clearLs = ()=>{
-    store =[];
-    localStorage.removeItem("itemLocal");
+let clearLs = () => {
+    store = [];
+    localStorage.removeItem("item-all");
+    setitem(getLs());
 }
 
 
 let listitem = document.querySelector('.list-group');
+let iconGet = document.querySelectorAll(".icon-get");
 
 
-let setitem = (item)=>{
-    listitem.innerHTML="";
-    console.log("item",item);
 
-    for(let val in item){
-        console.log("Name:",item[val].name);
-        
-        let litag=`<li class="list-group-item d-flex justify-content-between align-items-center task-item">
+let setitem = (item) => {
+    listitem.innerHTML = "";
+    console.log("item", item);
+
+    for (let val in item) {
+        console.log("Name:", item[val].name);
+
+        let litag = `<li class="list-group-item d-flex justify-content-between align-items-center task-item" data-time="${item[val].time}">
         <span class="task">${item[val].name}</span>
         <span class="task-item">
-        <a href="#"><i class="icon-get icon-green bi bi-bookmark-check"></i></a>
-        <a href="#"><i class="icon-get icon-gray bi bi-pen"></i></a>
-        <a href="#"><i class="icon-get icon-red bi bi-file-x"></i></a>
+        <a href="javascript:void(0)" data-check><i class="icon-get icon-green bi bi-bookmark-check"></i></a>
+        <a href="javascript:void(0)" data-edit><i class="icon-get icon-gray bi bi-pen"></i></a>
+        <a href="javascript:void(0)" data-delete><i class="icon-get icon-red bi bi-file-x"></i></a>
         </span>
         </li>`
-        listitem.insertAdjacentHTML("beforeend",litag)
+        listitem.insertAdjacentHTML("beforeend", litag)
     }
-    
-    
+    iconGet = document.querySelectorAll(".icon-get");
+    for (let i of iconGet) {
+        i.addEventListener('click', (e) => {
+            e.preventDefault();
+            //console.log(i)
+            i.classList.remove("bi-bookmark-check");
+            i.classList.add("bi-bookmark-check-fill");
+            let Checked = i.parentElement.parentElement.parentElement
+
+        });
+    }
+
 }
 
 let form = document.getElementsByTagName('form')[0];
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     setitem(getLs());
     form.addEventListener('submit', (e) => {
-        store=getLs();
+
+        store = (getLs() == null) ? [] : getLs();
         e.preventDefault();
         let inputText = document.getElementById('inputText').value.trim()
         if (inputText.length == 0) {
-            alert('Please input task')
-        }
-        else { 
-            const item={
-                name:inputText,
-                isDone:false,
+            alert('Please input task');
+        } else {
+            const item = {
+                name: inputText,
+                isDone: false,
                 time: new Date().getTime()
             }
             store.push(item);
